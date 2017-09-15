0c637e77c98bf10e0f845c7c493bfcb2768e5c73
Netanel Dziubov <netaneld122@gmail.com>
2017-09-16 00:58:36 +0300

    Made this a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='winsniffer',
+    version='0.1',
+    install_requires=['dpkt', 'hexdump', 'pypcap'],
+    packages=['winsniffer'],
+    url='https://bitbucket.org/netaneld122/winsniffer',
+    license='GPL License',
+    author='Netanel Dziubov',
+    description='Simple network sniffer for Windows',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ]
+)
---
+++ winsniffer/winsniffer.py
@@ -0,0 +1,8 @@
+import pcap
+import dpkt
+import hexdump
+import binascii
+
+
+class Sniffer(object):
+    pass
