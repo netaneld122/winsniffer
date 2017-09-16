a8d3c9c71af089bebe5e72e2fa7e077e35243f39
Netanel Dziubov <netaneld122@gmail.com>
2017-09-16 13:08:23 +0300

    Added semi proper background list population template, still gotta take care of the stop event

--- gui/winsniffer.py
+++ gui/winsniffer.py
@@ -1,5 +1,6 @@
 import wx
 import wx.py
+import threading
 import winsniffer
 
 from list_control import ListControl
@@ -30,22 +31,44 @@ class WinsnifferFrame(wx.Frame):
         # Center the window
         self.Center()
 
-        # TODO: Remove
-        for i in range(10):
-            index = self.list_control.InsertItem(self.list_control.GetItemCount(), str(i+1))
-            self.list_control.SetItem(index, 1, '0.0000')
-            self.list_control.SetItem(index, 2, '127.0.0.1')
-            self.list_control.SetItem(index, 3, '127.0.0.1')
-            self.list_control.SetItem(index, 4, 'Ethernet')
-            self.list_control.SetItem(index, 5, 'IP')
-            self.list_control.SetItem(index, 6, 'TCP')
-            self.list_control.SetItem(index, 7, '0x11 0x11 0x11 0x11 ...')
+        self.start_populating_list()
 
     @staticmethod
     def set_columns(list_control):
         columns = ('No.', 'Time', 'From', 'To', 'Data Link', 'Network', 'Transport', 'Data')
         for column_index, column in enumerate(columns):
             list_control.InsertColumn(column_index, column)
+
+    def start_populating_list(self):
+        thread = threading.Thread(target=self.populate_list)
+        thread.start()
+
+    def add_item(self, i):
+        index = self.list_control.InsertItem(self.list_control.GetItemCount(), str(i + 1))
+        self.list_control.SetItem(index, 1, '0.0000')
+        self.list_control.SetItem(index, 2, '127.0.0.1')
+        self.list_control.SetItem(index, 3, '127.0.0.1')
+        self.list_control.SetItem(index, 4, 'Ethernet')
+        self.list_control.SetItem(index, 5, 'IP')
+        self.list_control.SetItem(index, 6, 'TCP')
+        self.list_control.SetItem(index, 7, '0x11 0x11 0x11 0x11 ...')
+
+        # Auto scroll only when the user is scrolled to the bottom
+        self.smart_auto_scroll()
+
+    def smart_auto_scroll(self):
+        scroll_position = self.list_control.GetScrollPos(wx.VERTICAL)
+        visible_items_count = self.list_control.GetCountPerPage()
+        scroll_bottom_position = scroll_position + visible_items_count
+        item_count = self.list_control.GetItemCount()
+        if scroll_bottom_position >= item_count - 2:
+            self.list_control.EnsureVisible(item_count - 1)
+
+    def populate_list(self):
+        for i in range(1000):
+            import time
+            time.sleep(0.1)
+            wx.CallAfter(self.add_item, i)
 
 
 def main():
