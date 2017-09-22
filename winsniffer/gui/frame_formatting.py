5443edd1cb38962de45557fa1eef305e3d90cda0
Netanel Dziubov <netaneld122@gmail.com>
2017-09-22 23:04:26 +0300

    Added printable data to the data preview column, fixed the 1 byte null terminator display

--- winsniffer/gui/frame_formatting.py
+++ winsniffer/gui/frame_formatting.py
@@ -1,8 +1,13 @@
 import binascii
+import string
 
 
 def prettify_mac_address(mac_address):
     return ':'.join(map(binascii.hexlify, mac_address))
+
+
+def is_printable(_string):
+    return all(c in string.printable for c in _string)
 
 
 def get_frame_data_preview(frame):
@@ -11,10 +16,18 @@ def get_frame_data_preview(frame):
     while not isinstance(protocol, str):
         protocol = protocol.data
 
-    threshold = 20
+    threshold = 40
 
     data_preview = protocol[:min(threshold, len(protocol) - 1)]
-    data_preview = ' '.join(map(lambda s: binascii.hexlify(s).upper(), data_preview))
+
+    if protocol == '\x00':
+        data_preview = '00'
+    elif len(protocol) > 0 and is_printable(data_preview):
+        data_preview = unicode(data_preview)
+    else:
+        data_preview = ' '.join(map(lambda s: binascii.hexlify(s).upper(), data_preview))
+
+    # We truncated the data so add a ... indicator for that
     if len(protocol) > threshold:
         data_preview += "..."
 
