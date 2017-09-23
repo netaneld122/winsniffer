3a021636e0d9d61482c6449877788cbed54b1c0b
Netanel Dziubov <netaneld122@gmail.com>
2017-09-23 15:25:36 +0300

    Fixed icons retrieval when not in winsniffer working directory

--- winsniffer/gui/icons.py
+++ winsniffer/gui/icons.py
@@ -1,7 +1,14 @@
-SAVE = "icons/ic_save_white_24dp_2x.png"
-ADD = "icons/ic_playlist_add_white_24dp_2x.png"
-START = "icons/ic_play_arrow_white_24dp_2x.png"
-PAUSE = "icons/ic_pause_white_24dp_2x.png"
-AUTO_SCROLL = "icons/ic_vertical_align_bottom_white_24dp_2x.png"
-CLEAR = "icons/ic_delete_sweep_white_24dp_2x.png"
-FILTER = "icons/ic_filter_list_black_24dp_1x.png"
+import os
+
+
+def icon(filename):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', filename)
+
+
+SAVE = icon("ic_save_white_24dp_2x.png")
+ADD = icon("ic_playlist_add_white_24dp_2x.png")
+START = icon("ic_play_arrow_white_24dp_2x.png")
+PAUSE = icon("ic_pause_white_24dp_2x.png")
+AUTO_SCROLL = icon("ic_vertical_align_bottom_white_24dp_2x.png")
+CLEAR = icon("ic_delete_sweep_white_24dp_2x.png")
+FILTER = icon("ic_filter_list_black_24dp_1x.png")
