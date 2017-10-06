dc7f2ee7b01bc8af0f65e971318efd83c1b0dbcd
Netanel Dziubov <netaneld122@gmail.com>
2017-10-07 02:13:26 +0300

    Reverted --onefile into --onedir because it's better to know where the files are at

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ cd %~dp0
 rmdir /S /Q build
 rmdir /S /Q dist
 del winsniffer.spec
-pyinstaller winsniffer\gui\winsniffer_frame.py -n winsniffer --onefile --clean ^
+pyinstaller winsniffer\gui\winsniffer_frame.py -n winsniffer --onedir --clean ^
  --add-binary dependencies\pcap.pyd;. ^
  --add-binary %systemroot%\SysWOW64\wpcap.dll;. ^
  --add-binary winsniffer\gui\icons;icons ^
